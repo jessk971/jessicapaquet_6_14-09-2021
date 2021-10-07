ff4f7743b04e45e05d817b58d98f3cb4e746003d
Jessica PAQUET <jessica-paquet@live.fr>
2021-10-07 02:53:36 +0200

    controllers

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const mongoose = require('mongoose');
-const Sauce = require('./models/sauce');
 const sauceRoutes = require('./routes/sauce');
 
 
@@ -16,7 +15,7 @@ mongoose.connect('mongodb+srv://jessk971:Madinina971@cluster0.ergo0.mongodb.net/
 
 
 
-
+// CORS - partage de ressources entre serveurs
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -27,22 +26,8 @@ app.use((req, res, next) => {
 app.use(bodyParser.json());
 
 
+app.use('/api/sauces', sauceRoutes);
 
-app.post('/api/sauce', (req, res, next) => {
-    delete req.body._id;
-    const sauce = new Sauce({
-        ...req.body
-    });
-    sauce.save()
-        .then(() => res.status(201).json({ message: 'Objet enregistré !' }))
-        .catch(error => res.status(400).json({ error }));
-});
-
-app.get('/api/sauce', (req, res, next) => {
-    Sauce.find()
-        .then(things => res.status(200).json(sauce))
-        .catch(error => res.status(400).json({ error }));
-});
 
 
 module.exports = app;
--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -1,1 +1,13 @@
 const express = require('express');
+const router = express.Router();
+const Sauce = require('../models/sauce');
+
+const sauceCtrl = require('../controllers/sauce');
+
+router.get('/', sauceCtrl.getAllSauces);
+
+
+
+
+
+module.exports = router;
